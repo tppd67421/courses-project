cedad46f88f0b49f8f4be7f5b259d6682fa63ce1
Aleksey <tppd67421@gmail.com>
2019-04-04 01:10:37 +0300

    create header

--- ui/src/app/components/App.tsx
+++ ui/src/app/components/App.tsx
@@ -1,38 +1,22 @@
 import * as React from 'react';
 import '../../assets/styles/main.scss';
 import SideBar from './SideBar';
+import Header from './Header';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import CoursesFlow from './CoursesFlow';
 import { IAppState } from '../store/reducers';
 import { connect } from 'react-redux';
 import { SharedService } from '../services/sharedService';
 
-interface Props {
-    children: React.ReactNode;
-    test?: boolean;
-    testFn?: () => void;
-}
 
-const mapStateToProps = (state: IAppState, props: Props): Partial<Props> => {
+const mapStateToProps = (state: IAppState, props: any): Partial<any> => {
     return {
         ...props,
         test: state.shared.test,
     };
 };
 
-const mapDispatchToProps = (dispatch: any, props: Props): Partial<Props> => {
-    return {
-        ...props,
-        testFn: () => {
-            dispatch(SharedService.test());
-        },
-    };
-};
-
-const App = (props: Props): JSX.Element => {
-    const onClick = () => {
-        props.testFn();
-    };
+const App = (props: any): JSX.Element => {
 
     return (
         <div className='cc-app'>
@@ -41,7 +25,7 @@ const App = (props: Props): JSX.Element => {
                       <SideBar />
                 </div>}
             <div className='cc-app__header'>
-             <button onClick={onClick}>TEST</button>
+            <Header />
             </div>
             <div className='cc-app__main'>
                 <Switch>
@@ -54,5 +38,5 @@ const App = (props: Props): JSX.Element => {
 
 export default connect(
     mapStateToProps,
-    mapDispatchToProps,
+    null,
 )(App);
---
+++ ui/src/app/components/Header/index.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { IAppState } from '../../store/reducers';
+import { SharedService } from '../../services/sharedService';
+import { connect } from 'react-redux';
+import './index.scss';
+
+interface Props {
+    children: React.ReactNode;
+    test?: boolean;
+    testFn?: () => void;
+}
+
+const mapStateToProps = (state: IAppState, props: Props): Partial<Props> => {
+    return {
+        ...props,
+        test: state.shared.test,
+    };
+};
+
+const mapDispatchToProps = (dispatch: any, props: Props): Partial<Props> => {
+    return {
+        ...props,
+        testFn: () => {
+            dispatch(SharedService.test());
+        },
+    };
+};
+
+const Header = (props: Props): JSX.Element => {
+    const onClick = () => {
+        props.testFn();
+    };
+
+    return (
+        <div className='cc-header-wrapper'>
+            <div className='cc-header'>
+                <ul className='cc-header-left'>
+                    <li className='cc-header-button' onClick={onClick}></li>
+                    <li className='cc-header-navigation__item'>TestingCourses</li>
+                </ul>
+                <ul className="cc-header-right">
+                    <li><input type="text" placeholder='Поиск курсов' /></li>
+                    <li><div className="like">like</div></li>
+                    <li><div className="auth">auth</div></li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(Header);
